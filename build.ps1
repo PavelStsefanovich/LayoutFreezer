f7f0407b521b79c4ba2d6286d3f970d5c32f30f0
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2021-01-22 13:50:26 -0800

    1.Added build file; 2.Updated .gitignore

---
+++ build.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = 'Stop'
+
+$current_location = $PWD.Path
+cd $PSScriptRoot
+
+try {
+    gi dist -ErrorAction SilentlyContinue | rm -Force -recurse
+    & pipenv install
+    & pipenv run pyinstaller -F -i snap.ico -n BookScraper main.py
+    # cp params.yml dist
+    # cp config dist -recurse
+    # rm dist/config/*.ico    
+}
+catch {
+    throw $_
+}
+finally {
+    write "BUILD FINISHED"
+    cd $current_location
+}

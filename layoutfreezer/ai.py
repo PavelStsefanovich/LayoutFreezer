14cd7335679d56db0edfb4e4266c880abc09dd1f
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2021-02-17 00:33:14 -0800

    Implemented get_closest_match() function to compare configurations with patially matching titles

--- layoutfreezer/ai.py
+++ layoutfreezer/ai.py
@@ -11,6 +11,7 @@ for the apps with partial match (when process name matches,
 but screen layout or title is different)
 '''
 
+from difflib import get_close_matches
 from layoutfreezer.helpers import get_config_matches
 import logging
 
@@ -22,8 +23,15 @@ logger = logging.getLogger(__name__)
 
 ##########  Functions  ############################
 
-def get_closest_match(normalized_config, matches):
-    #TODO implement
+def get_closest_match(normalized_config, matches, cutoff):
+    curr_title = normalized_config[1]
+    compare_titles = list(zip(*matches))[1]
+    closest_match_title = get_close_matches(
+        curr_title, compare_titles, n=1, cutoff=cutoff)
+    if closest_match_title:
+        for m in matches:
+            if m[1] == closest_match_title[0]:
+                return m
     return normalized_config
 
 
@@ -44,7 +52,10 @@ def adjust_window_rectangle(
         if isinstance(matches, dict):
             # partial matches
             closest_match = get_closest_match(
-                normalized_config, matches['partial_matches'])
+                normalized_config,
+                matches['partial_matches'],
+                prefs['match_cutoff']
+            )
             adjusted_rect = convert_rectangle_to_int(closest_match[2])
         else:
             # full match

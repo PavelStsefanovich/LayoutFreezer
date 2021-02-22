443cdadfa45ddd97bae43e6d17c8d99ef04c9f9b
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2021-02-21 17:02:02 -0800

    1.Included version into the package name; 2.Excluded base directory from io.compression.zipfile

--- build.ps1
+++ build.ps1
@@ -10,12 +10,20 @@ try {
     & pipenv install
     & pipenv run pyinstaller -F -i layoutfreezer.ico -n LayoutFreezer --windowed main.py
     # package
+    $major_version = '0.0.0'
+    foreach ($line in (cat .\config.yml)) {
+        if ($line -match '^version\:\s(\d+\.\d+\.\d+)$') {
+            $major_version = $matches[1]; break
+        }
+    }
     mkdir .\dist\LayoutFreezer | Out-Null
     foreach ($file in @('dist\LayoutFreezer.exe', 'config.yml', 'prefs.yml', 'layoutfreezer.png', 'logger.json', 'README.md')) {
         (gi $file).FullName | % { cp $_ .\dist\LayoutFreezer -Force }
     }
     Add-Type -AssemblyName "system.io.compression.filesystem"
-    [io.compression.zipfile]::CreateFromDirectory('.\dist\LayoutFreezer', 'LayoutFreezer.zip', "Optimal", $true)
+    $source_dir = Join-Path $PSScriptRoot 'dist\LayoutFreezer'
+    $output_zip = Join-Path $PSScriptRoot "LayoutFreezer-$major_version.zip"
+    [io.compression.zipfile]::CreateFromDirectory($source_dir, $output_zip, "Optimal", $false)
 }
 catch {
     throw $_
